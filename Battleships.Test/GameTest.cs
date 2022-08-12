0947abf89fb36cdc0702ec407a4a83905bfa0e4c
Beno Anto Victor <benovictor@outlook.com>
2022-08-12 12:18:25 -0400

    Code with unit test

--- Battleships.Test/GameTest.cs
+++ Battleships.Test/GameTest.cs
@@ -17,5 +17,39 @@ namespace Battleships.Test
             var guesses = new[] { "7:0", "3:3" };
             Game.Play(ships, guesses).Should().Be(0);
         }
+
+        [Fact]
+        public void TestPlay_SunkOneShip()
+        {
+            var ships = new[] { "3:2,3:5" };
+            var guesses = new[] { "7:0", "3:3", "3:4", "3:5", "3:2" };
+            Game.Play(ships, guesses).Should().Be(1);
+        }
+
+        [Fact]
+        public void TestPlay_Sunk2Ship()
+        {
+            var ships = new[] { "3:2,3:5", "7:0,6:0"};
+            var guesses = new[] { "7:0", "3:3", "3:4", "3:5", "3:2", "6:0" };
+            Game.Play(ships, guesses).Should().Be(2);
+        }
+
+        [Fact]
+        public void TestPlay_WithOneInvalidShip()
+        {
+            //Invalid ship
+            var ships = new[] { "3:2,3:5", "3:6,4:7" };
+            var guesses = new[] { "7:0", "3:3" };
+            Game.Play(ships, guesses).Should().Be(0);
+        }
+
+        [Fact]
+        public void TestPlay_ShipWithInvalidLength()
+        {
+            //Invalid Ship Length
+            var ships = new[] { "3:2,3:6", "7:0,6:0" };
+            var guesses = new[] { "7:0", "3:3", "3:4", "3:5", "3:2", "3:6", "6:0" };
+            Game.Play(ships, guesses).Should().Be(1);
+        }
     }
 }
--- Battleships/Game.cs
+++ Battleships/Game.cs
@@ -20,7 +20,191 @@ namespace Battleships
         // returns: the number of ships sunk by the set of guesses
         public static int Play(string[] ships, string[] guesses)
         {
-            return 0;
+            var shipsLt = new List<Ship>();
+            foreach (var sh in ships)
+            {
+                if(Ship.TryCreateShip(sh, out var ship, out var errorMsg))
+                {
+                    shipsLt.Add(ship);
+                }
+                else
+                {
+                    Console.WriteLine(errorMsg);
+                }
+            }
+
+            //Hit the ships
+
+            int hit = 0;
+
+            foreach(var guess in guesses)
+            {
+               foreach(var ship in shipsLt)
+               {
+                    ship.Hit(guess);
+               }
+            }
+
+            //Get the ships sunk
+            var shipsSunk = shipsLt.Where(s => s.IsSunk());
+
+            return shipsSunk.Count();
         }
+    }
+
+    public class ShipCoordinate
+    {
+        public bool IsHit { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Ship
+    {
+        
+
+        private readonly ShipCoordinate _c1;
+        private readonly ShipCoordinate _c2;
+
+        private readonly Dictionary<string, ShipCoordinate> _coordinateArrayLt;
+
+
+        public void Hit(string coordinate)
+        {
+            if(_coordinateArrayLt.ContainsKey(coordinate))
+            {
+                _coordinateArrayLt[coordinate].IsHit = true;
+            }
+        }
+
+        public bool IsSunk()
+        {
+            //if any of the coordinate is not hit then ship is staying afloat
+            if (_coordinateArrayLt.Any(kv => !kv.Value.IsHit))
+                return false;
+            return true;
+        }
+
+        private Ship(ShipCoordinate c1, ShipCoordinate c2)
+        {
+            _c1 = c1;
+            _c2 = c2;
+            _coordinateArrayLt = new Dictionary<string, ShipCoordinate>();
+            if (_c1.X == _c2.X)
+            {
+                for(int i=_c1.Y; i <= _c2.Y; i++)
+                {
+                    _coordinateArrayLt.Add( $"{_c1.X}:{i}", new ShipCoordinate { X = _c1.X, Y = i });
+                }
+            }
+            if (_c1.Y == _c2.Y)
+            {
+                for (int i = _c1.X; i <= _c2.X; i++)
+                {
+                    _coordinateArrayLt.Add($"{i}:{_c1.Y}" ,new ShipCoordinate { X = i, Y = _c1.Y });
+                }
+            }
+        }
+
+        /// <summary>
+        /// Parses the ship co-ordinate and validates the ships size and then creates ship object
+        /// </summary>
+        /// <param name="ships"></param>
+        /// <param name="ship"></param>
+        /// <param name="errorMsg"></param>
+        /// <returns></returns>
+        public static bool TryCreateShip(string shipCoordinate, out Ship ship, out string errorMsg)
+        {
+            bool isSuccessFul = false;
+            errorMsg = string.Empty;
+            var coordinateArray = shipCoordinate.Split(',');
+
+            //Parse the co-ordinates
+            var typeCoordinate = coordinateArray.Select(c =>
+            {
+                var data = c.Split(':');
+                if(data.Length == 2)
+                {
+                   
+                    if (int.TryParse(data[0], out int x))
+                    {
+                        if (int.TryParse(data[1], out int y))
+                        {
+                            return new ShipCoordinate { X = x, Y = y };
+                        }
+                    }
+                    
+                }
+
+                return null;
+
+            }).Where(c => c != null).ToArray();
+
+            //if there are less than 2 typed coordinates then something wrong with the shipCoordinate string
+            if(typeCoordinate.Length == 2)
+            {
+                //Check the size of the ship
+                //one unit wide and 2-4 units long
+                var c1 = typeCoordinate[0];
+                var c2 = typeCoordinate[1];
+
+                bool isSizeValid = false;
+
+                int len = 0;
+
+                if (c1.X == c2.X) //Parked Horizontally
+                {
+                    len = Math.Abs(c2.Y - c1.Y) + 1;// - 1);
+
+                    if (len >= 1 && len <= 4)
+                    {
+                        isSizeValid = true;
+                    }
+                    else
+                    {
+                        errorMsg = $"Invalid Ship Length {len}: Ship {shipCoordinate}";
+                    }
+
+                }
+                else if (c1.Y == c2.Y) // Parked Vertically
+                {
+                    len = Math.Abs(c2.X - c1.X) + 1;// - 1);
+
+                    if (len >= 1 && len <= 4)
+                    {
+                        isSizeValid = true;
+                    }
+                    else
+                    {
+                        errorMsg = $"Invalid Ship Length {len}: Ship {shipCoordinate}";
+                    }
+                }
+                else
+                {
+                    isSizeValid = false;
+
+                    errorMsg = $"Invalid Invalid Ship-Coordinate value : {shipCoordinate}. Neither parked Horizontally or Vertically";
+                }
+
+                if(isSizeValid)
+                {
+                    ship = new Ship(typeCoordinate[0], typeCoordinate[1]);
+                    isSuccessFul = true;
+                    errorMsg = string.Empty;
+
+                    return isSuccessFul;
+                }
+                
+            }
+            
+            ship = null;
+            errorMsg = string.IsNullOrWhiteSpace(errorMsg)?  $"Invalid Ship-Coordinate value : {shipCoordinate}": errorMsg;
+            
+
+            return isSuccessFul;
+        }
+
+        
     }
 }
